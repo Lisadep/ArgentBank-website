5c33e10a1227dea117addbaf246d06bdb69e5775
Lisa de Pierrepont <depierrepont.lisa@gmail.com>
2023-10-04 10:48:15 +0200

    Main page

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,13 +1,20 @@
+import './style/main.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './pages/Home'
+import User from './pages/User'
+// import SignIn from './pages/SingIn'
+import Footer from './components/footer'
 
 const App = () => {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/user" element={<User />}/>
+          {/* <Route path="/signin" element={<SignIn />}/> */}
         </Routes>
+        <Footer />
     </Router>
   );
 };
