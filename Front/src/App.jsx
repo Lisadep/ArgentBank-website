4861ececdce008b540952c67af8cb18d0e4867d4
Lisa de Pierrepont <depierrepont.lisa@gmail.com>
2023-10-10 12:47:34 +0200

    page error

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,20 +1,25 @@
-import './style/main.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Header from './components/header'
 import Home from './pages/Home'
 import User from './pages/User'
-// import SignIn from './pages/SingIn'
+import SignIn from './pages/SignIn'
 import Footer from './components/footer'
+import Error from './pages/Error'
 
 const App = () => {
+  const token = useSelector(state => state.signIn.token)
   return (
     <Router>
+      <Header />
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/user" element={<User />}/>
-          {/* <Route path="/signin" element={<SignIn />}/> */}
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/user" element={token ? <User /> : <Navigate to="/sign-in" />} />
+          <Route path="*" element={<Error />} />
         </Routes>
-        <Footer />
+      <Footer />
     </Router>
   );
 };
