28201699d2dbef78917640ba44dde4088202c4af
Lisa de Pierrepont <depierrepont.lisa@gmail.com>
2023-11-01 17:14:15 +0100

    doc swagger

--- Front/src/index.js
+++ Front/src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import '../src/style/main.css'
-import 'font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
