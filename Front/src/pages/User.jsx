a8dc5c3c0390f425b8c36beda5372d227e5f742e
Lisa de Pierrepont <depierrepont.lisa@gmail.com>
2023-11-12 13:41:17 +0100

    add compo account

--- Front/src/pages/User.jsx
+++ Front/src/pages/User.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { getUserData } from "../redux/store"
 import callAPI from "../services/apiServices"
 import EditForm from "../components/editForm"
+import Account from "../components/account"
 
 function User() {
   const token = useSelector((state) => state.signIn.token) // Récupération de la valeur du token stocké dans le slice signIn
@@ -33,36 +34,21 @@ function User() {
       </div>
       <EditForm />
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <Account
+        title="Argent Bank Checking (x8349)"
+        amount="$2,082.79"
+        text="Available Balance"
+      />
+      <Account
+        title="Argent Bank Savings (x6712)"
+        amount="$10,928.42"
+        text="Available Balance"
+      />
+      <Account
+        title="Argent Bank Credit Card (x8349)"
+        amount="$184.30"
+        text="Current Balance"
+      />
     </main>
   )
 }
